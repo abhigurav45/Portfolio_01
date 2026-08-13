7116bd0c05c79c7d7675afb57e22f69b0723bab3
Abhishek Gurav <abhishekgurav703@gmail.com>
2026-08-13 21:52:27 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -4,8 +4,7 @@
  import Project from "./pages/Project/Project"
  import Services from "./pages/Services/Services"
 
- import Contact from "./pages/Contact/Contact"
-import Footer from "./pages/Footer/Footer"
+ import Footer from "./pages/Footer/Footer"
  function App() {
 
   
