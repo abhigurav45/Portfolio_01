3cdf141c677fc419a00a150cc8f9ee7c818c5720
Abhishek Gurav <abhishekgurav703@gmail.com>
2026-08-22 09:58:11 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -4,7 +4,9 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    
+    createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
  
 
      </StrictMode>,
