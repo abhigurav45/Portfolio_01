bfa2374e37b3af7d959adda8d611277a4330a826
Abhishek Gurav <abhishekgurav703@gmail.com>
2026-09-04 19:44:21 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -3,7 +3,7 @@ import Hero from "./pages/Hero/Hero"
 
  import About from "./pages/About/About"
  import Project from "./pages/Project/Project" 
- 
+  import Services from "./pages/Services/Services"
  import Contact from "./pages/Contact/Contact"
   import Footer frm "./pages/Footer/Footer"
   return (
